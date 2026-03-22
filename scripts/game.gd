79887826b09ada04cc285c31449f6ed4925fc69c
Josh Hartley <volcanoem@hotmail.co.uk>
2026-03-22 20:31:45 +0000

    feat: ball go faster

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
-var _volley_count: int = 0
+@export var ball: RigidBody2D
+@export var hud: CanvasLayer
+@export var paddle: Node
+
+var _volley_count := 0
 
 
 func _ready() -> void:
-	$Ball.paddle_hit.connect(_on_paddle_hit)
+	print("game ready")
+	paddle.paddle_hit.connect(_on_paddle_hit)
 
 
 func _on_paddle_hit() -> void:
 	_volley_count += 1
-	$CanvasLayer/Label.text = "Volleys: %d" % _volley_count
+	hud.update_volley_count(_volley_count)
+	ball.increase_speed()
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,6 +1,11 @@
 extends RigidBody2D
 
+signal paddle_hit
+
+@export var hit_sound: AudioStreamPlayer
+
 var _start_x: float
+var _streak := 0
 
 
 func _ready() -> void:
@@ -13,3 +18,14 @@ func _physics_process(_delta: float) -> void:
 	var direction := Input.get_axis("paddle_up", "paddle_down")
 	linear_velocity.y = direction * GameRules.PADDLE_SPEED
 	position.x = _start_x  # Lock x position
+
+
+func on_ball_hit() -> void:
+	_streak += 1
+	hit_sound.pitch_scale = 1.0 + (_streak * 0.05)
+	hit_sound.play()
+	paddle_hit.emit()
+
+
+func reset_streak() -> void:
+	_streak = 0
