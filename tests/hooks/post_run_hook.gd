4ab48ddb9aa9940b955fec3f6d5ec34a9218a25c
Josh Hartley <volcanoem@hotmail.co.uk>
2026-03-23 05:08:22 +0000

    feat: coverage testing

---
+++ tests/hooks/post_run_hook.gd
@@ -0,0 +1,34 @@
+extends "res://addons/gut/hook_script.gd"
+
+const Coverage = preload("res://addons/coverage/coverage.gd")
+
+const COVERAGE_TARGET := 50.0
+const FILE_TARGET := 25.0
+
+
+func run():
+	var coverage := Coverage.instance
+	var coverage_file := (
+		OS.get_environment("COVERAGE_FILE") if OS.has_environment("COVERAGE_FILE") else ""
+	)
+	if coverage_file:
+		coverage.save_coverage_file(coverage_file)
+	coverage.set_coverage_targets(COVERAGE_TARGET, FILE_TARGET)
+	var verbosity := Coverage.Verbosity.FAILING_FILES
+	var logger := gut.get_logger()
+	coverage.finalize(verbosity)
+	if coverage.coverage_passing():
+		logger.passed(
+			(
+				"Coverage: %.1f%% total (target %.1f%%), %.1f%% file (target %.1f%%)"
+				% [coverage.coverage_percent(), COVERAGE_TARGET, FILE_TARGET, FILE_TARGET]
+			)
+		)
+	else:
+		logger.failed(
+			(
+				"Coverage target of %.1f%% total (%.1f%% file) was not met"
+				% [COVERAGE_TARGET, FILE_TARGET]
+			)
+		)
+		set_exit_code(2)
---
+++ tests/hooks/pre_run_hook.gd
@@ -0,0 +1,8 @@
+extends "res://addons/gut/hook_script.gd"
+
+const Coverage = preload("res://addons/coverage/coverage.gd")
+const EXCLUDE_PATHS = ["res://addons/*", "res://tests/*"]
+
+
+func run():
+	Coverage.new(gut.get_tree(), EXCLUDE_PATHS).instrument_scripts("res://")
