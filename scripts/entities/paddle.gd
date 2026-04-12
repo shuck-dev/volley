8ea36257778e0d5f26469ffed2e874e188368d18
Josh <josh@hartley.best>
2026-04-12 20:45:33 +0100

    SH-85 fix: virtual methods to keep partner paddle class clean

--- scripts/entities/paddle.gd
+++ scripts/entities/paddle.gd
@@ -18,12 +18,9 @@ var _sprite_natural_height := 0.0
 
 
 func _ready() -> void:
-	if _item_manager == null:
-		_item_manager = ItemManager
-
 	_lane_x = position.x
-	_paddle_speed = _item_manager.get_stat(&"paddle_speed")
-	_item_manager.item_level_changed.connect(_on_item_level_changed)
+	_paddle_speed = _get_stat(&"paddle_speed")
+	_bind_stat_updates()
 
 	if collision != null:
 		_collision_shape = RectangleShape2D.new()
@@ -59,20 +56,30 @@ func get_speed() -> float:
 	return _paddle_speed
 
 
+func _get_stat(key: StringName) -> float:
+	if _item_manager == null:
+		_item_manager = ItemManager
+	return _item_manager.get_stat(key)
+
+
+func _bind_stat_updates() -> void:
+	if _item_manager == null:
+		_item_manager = ItemManager
+	_item_manager.item_level_changed.connect(_on_item_level_changed)
+
+
 func _on_item_level_changed(_item_key: String) -> void:
 	_apply_size()
-	_paddle_speed = _item_manager.get_stat(&"paddle_speed")
+	_paddle_speed = _get_stat(&"paddle_speed")
 
 
 func _apply_size() -> void:
 	if _collision_shape == null:
 		return
 
-	var arena_height: float = _item_manager.get_stat(&"arena_height")
-	var paddle_size_min: float = _item_manager.get_stat(&"paddle_size_min")
-	var new_size: float = clampf(
-		_item_manager.get_stat(&"paddle_size"), paddle_size_min, arena_height
-	)
+	var arena_height: float = _get_stat(&"arena_height")
+	var paddle_size_min: float = _get_stat(&"paddle_size_min")
+	var new_size: float = clampf(_get_stat(&"paddle_size"), paddle_size_min, arena_height)
 
 	_collision_shape.size.y = new_size
 
--- scripts/entities/partner_paddle.gd
+++ scripts/entities/partner_paddle.gd
@@ -4,35 +4,12 @@ extends Paddle
 @export var controller: PartnerAIController
 
 
-func _ready() -> void:
-	_lane_x = position.x
-	_paddle_speed = GameRules.base_stats[&"paddle_speed"]
-
-	if collision != null:
-		_collision_shape = RectangleShape2D.new()
-		_collision_shape.size = collision.shape.size
-		collision.shape = _collision_shape
-
-	if sprite != null:
-		_sprite_natural_height = sprite.get_rect().size.y
-
-	_apply_base_size()
+func _get_stat(key: StringName) -> float:
+	return GameRules.base_stats[key]
 
 
-func _apply_base_size() -> void:
-	if _collision_shape == null:
-		return
-
-	var arena_height: float = GameRules.base_stats[&"arena_height"]
-	var paddle_size_min: float = GameRules.base_stats[&"paddle_size_min"]
-	var new_size: float = clampf(
-		GameRules.base_stats[&"paddle_size"], paddle_size_min, arena_height
-	)
-
-	_collision_shape.size.y = new_size
-
-	if sprite != null and _sprite_natural_height > 0.0:
-		sprite.scale.y = new_size / _sprite_natural_height
+func _bind_stat_updates() -> void:
+	pass
 
 
 func set_ball(value: RigidBody2D) -> void:
