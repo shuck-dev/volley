b01d471001ef57aa2ba4dbfe43fb9caadaa30782
Josh <josh@hartley.best>
2026-06-16 15:20:52 +0100

    refactor: align venue parallax layers with tech-art pipeline
    Agent-Role: gdscript-implementer
    Signed-off-by: Josh <josh@hartley.best>

--- scripts/core/venue_parallax.gd
+++ scripts/core/venue_parallax.gd
@@ -1,29 +1,35 @@
 class_name VenueParallax
 extends Node2D
 
-@export_group("Deep Background")
-@export var deep_color: Color = Color(0.05, 0.05, 0.12)
-@export_range(0.0, 1.0) var deep_scroll_x: float = 0.05
-@export_range(0.0, 1.0) var deep_scroll_y: float = 0.05
+@export_group("Layer 01: Deep Background")
+@export var deep_background_color: Color = Color(0.05, 0.05, 0.12)
+@export_range(0.0, 1.0) var deep_background_scroll_x: float = 0.05
+@export_range(0.0, 1.0) var deep_background_scroll_y: float = 0.05
 
-@export_group("Background")
-@export var bg_color: Color = Color(0.1, 0.1, 0.2)
-@export_range(0.0, 1.0) var bg_scroll_x: float = 0.2
-@export_range(0.0, 1.0) var bg_scroll_y: float = 0.2
+@export_group("Layer 02: Background")
+@export var background_color: Color = Color(0.1, 0.1, 0.2)
+@export_range(0.0, 1.0) var background_scroll_x: float = 0.2
+@export_range(0.0, 1.0) var background_scroll_y: float = 0.2
 
-@export_group("Midground")
-@export var mid_color: Color = Color(0.15, 0.14, 0.26)
-@export_range(0.0, 1.0) var mid_scroll_x: float = 0.5
-@export_range(0.0, 1.0) var mid_scroll_y: float = 0.5
+@export_group("Layer 04: Near Foreground")
+@export var near_foreground_color: Color = Color(0.18, 0.16, 0.28)
+@export_range(0.5, 2.0) var near_foreground_scroll_x: float = 1.3
+@export_range(0.5, 2.0) var near_foreground_scroll_y: float = 1.3
+
+@export_group("Layer 05: Foreground")
+@export var foreground_color: Color = Color(0.22, 0.2, 0.32)
+@export_range(0.5, 3.0) var foreground_scroll_x: float = 1.8
+@export_range(0.5, 3.0) var foreground_scroll_y: float = 1.8
 
 
 func _ready() -> void:
-	_add_parallax_layer(deep_color, Vector2(deep_scroll_x, deep_scroll_y))
-	_add_parallax_layer(bg_color, Vector2(bg_scroll_x, bg_scroll_y))
-	_add_parallax_layer(mid_color, Vector2(mid_scroll_x, mid_scroll_y))
+	_add_layer(deep_background_color, Vector2(deep_background_scroll_x, deep_background_scroll_y))
+	_add_layer(background_color, Vector2(background_scroll_x, background_scroll_y))
+	_add_layer(near_foreground_color, Vector2(near_foreground_scroll_x, near_foreground_scroll_y))
+	_add_layer(foreground_color, Vector2(foreground_scroll_x, foreground_scroll_y))
 
 
-func _add_parallax_layer(color: Color, scroll_scale: Vector2) -> void:
+func _add_layer(color: Color, scroll_scale: Vector2) -> void:
 	var parallax := Parallax2D.new()
 	parallax.follow_viewport = true
 	parallax.scroll_scale = scroll_scale
