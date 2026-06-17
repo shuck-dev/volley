575b83329225c6e8c86b193e11cd04923166597f
Josh <josh@hartley.best>
2026-06-17 22:01:49 +0100

    feat: grounded paddle racket crouch on paddle_down
    Agent-Role: gdscript-implementer
    Signed-off-by: Josh <josh@hartley.best>

--- scripts/entities/player_paddle.gd
+++ scripts/entities/player_paddle.gd
@@ -1,6 +1,15 @@
 class_name PlayerPaddle
 extends Paddle
 
+var _base_racket_y: float = 0.0
+
+
+func _ready() -> void:
+	super()
+
+	if racket_hitbox != null:
+		_base_racket_y = racket_hitbox.position.y
+
 
 func _physics_move(_delta: float) -> void:
 	var direction := Input.get_axis("paddle_up", "paddle_down")
@@ -8,3 +17,15 @@ func _physics_move(_delta: float) -> void:
 	move_and_slide()
 	position.x = _lane_x
 	clamp_to_arena()
+
+	if racket_hitbox == null or _body_shape == null or _racket_shape == null:
+		return
+
+	if _is_grounded() and Input.is_action_pressed("paddle_down"):
+		racket_hitbox.position.y = (
+			collision.position.y + _body_shape.size.y * 0.5 - _racket_shape.size.y * 0.5
+		)
+	else:
+		racket_hitbox.position.y = _base_racket_y
+
+	_refresh_overlay_shapes()
