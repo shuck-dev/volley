02cb96b1194871764d432f4ffb51ab0ec40bb1b3
Josh <josh@hartley.best>
2026-06-24 11:41:43 +0100

    refactor: remove dev offset from player paddle
    Agent-Role: gdscript-implementer
    Signed-off-by: Josh <josh@hartley.best>

--- scripts/entities/player_paddle.gd
+++ scripts/entities/player_paddle.gd
@@ -4,7 +4,6 @@ extends Paddle
 @export var low_anchor: Marker2D
 
 var _default_racket_position: Vector2
-var _development_offset := Vector2.ZERO
 var _low_states := [&"ready_grounded_low", &"swing_grounded_low"]
 
 
@@ -33,24 +32,12 @@ func _on_animation_state_changed(state: StringName) -> void:
 		return
 
 	if state in _low_states:
-		racket_hitbox.position = low_anchor.position + _development_offset
+		racket_hitbox.position = low_anchor.position
 	else:
-		racket_hitbox.position = _default_racket_position + _development_offset
+		racket_hitbox.position = _default_racket_position
 
 	_refresh_overlay_shapes()
 
 
 func _is_crouching() -> bool:
 	return is_grounded() and Input.is_action_pressed("paddle_down")
-
-
-func set_racket_position_x(offset_x: float) -> void:
-	_development_offset.x = offset_x
-	var state := get_movement_state()
-	_on_animation_state_changed(state)
-
-
-func set_racket_position_y(offset_y: float) -> void:
-	_development_offset.y = offset_y
-	var state := get_movement_state()
-	_on_animation_state_changed(state)
