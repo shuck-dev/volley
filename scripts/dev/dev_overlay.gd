6f3b301b8df79a927ff44ce042fa11d77cb94b8c
Josh <josh@hartley.best>
2026-06-24 13:07:59 +0100

    refactor: DevOverlay as direct Node2D, no scene instance
    Agent-Role: gdscript-implementer
    Signed-off-by: Josh <josh@hartley.best>

--- scripts/dev/dev_overlay.gd
+++ scripts/dev/dev_overlay.gd
@@ -7,64 +7,45 @@ extends Node2D
 @export var sprite: AnimatedSprite2D
 @export var ground_ray: RayCast2D
 
-@export var body_collider: BodyColliderOverlay
-@export var racket_collider: RacketColliderOverlay
-@export var ray_overlay: GroundRayOverlay
-@export var state_label: AnimationStateLabel
+@onready var body_collider: BodyColliderOverlay = $BodyColliderOverlay
+@onready var racket_collider: RacketColliderOverlay = $RacketColliderOverlay
+@onready var ray_overlay: GroundRayOverlay = $GroundRayOverlay
+@onready var state_label: AnimationStateLabel = $AnimationStateLabel
 
 
 func _ready() -> void:
 	if not OS.is_debug_build():
-		print("DevOverlay: freeing, not debug build")
 		queue_free()
 		return
 
-	print(
-		"DevOverlay._ready: collision=",
-		collision != null,
-		" body_collider=",
-		body_collider != null,
-		" state_label=",
-		state_label != null
-	)
-
-	if body_collider != null:
-		body_collider.collision = collision
-		body_collider.queue_redraw()
-	if racket_collider != null:
-		racket_collider.racket_hitbox = racket_hitbox
-		racket_collider.racket_shape = racket_shape
-		racket_collider.queue_redraw()
-	if ray_overlay != null:
-		ray_overlay.ground_ray = ground_ray
-		ray_overlay.queue_redraw()
-	if state_label != null and sprite != null:
-		state_label.sprite = sprite
-		state_label.collision = collision
-		sprite.animation_changed.connect(state_label._refresh)
-		state_label._refresh()
+	body_collider.collision = collision
+	body_collider.queue_redraw()
+	racket_collider.racket_hitbox = racket_hitbox
+	racket_collider.racket_shape = racket_shape
+	racket_collider.queue_redraw()
+	ray_overlay.ground_ray = ground_ray
+	ray_overlay.queue_redraw()
+	state_label.sprite = sprite
+	state_label.collision = collision
+	sprite.animation_changed.connect(state_label._refresh)
+	state_label._refresh()
 
 
 func _physics_process(_delta: float) -> void:
-	if ray_overlay != null:
-		ray_overlay.queue_redraw()
+	ray_overlay.queue_redraw()
 
 
 func set_body_collider_visible(shown: bool) -> void:
-	if body_collider != null:
-		body_collider.visible = shown
+	body_collider.visible = shown
 
 
 func set_racket_collider_visible(shown: bool) -> void:
-	if racket_collider != null:
-		racket_collider.visible = shown
+	racket_collider.visible = shown
 
 
 func set_ground_ray_visible(shown: bool) -> void:
-	if ray_overlay != null:
-		ray_overlay.visible = shown
+	ray_overlay.visible = shown
 
 
 func set_state_label_visible(shown: bool) -> void:
-	if state_label != null:
-		state_label.visible = shown
+	state_label.visible = shown
